064f35680bc45155f512e95e2dc6323beedd4e25
yuki <yukiiso0105@icloud.com>
2021-10-22 00:06:25 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User
     def initialize
         @first_name = "Yuki"
         @last_name = "Isono"
-        @birthday = "19991/5"
+        @birthday = "1999/1/5"
         @age = 22
         @birthplace = "chiba"
         @hobby = "Video Game"
@@ -12,7 +12,7 @@ class User
         <<~EOS
 
         私の名前は#{@first_name + @last_name}です。
-        誕生日は#{@birthday}で、年齢は#{@age}です。
+        誕生日は#{@birthday}で、年齢は#{@age}歳です。
         出身地は#{@birthplace}で、趣味は#{@hobby}です。
 
         EOS
